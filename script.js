0ed8074c0baa05cc5d48fcdbb2ac67e28c9caddc
Patuan72 <ozoedtech@gmail.com>
2025-07-18 01:03:25 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-document.getElementById("file-input").addEventListener("change", function (e) {
-  const file = e.target.files[0];
-  if (file) {
-    document.getElementById("filename-display").textContent = file.name;
-    const reader = new FileReader();
-    reader.onload = function (event) {
-      document.getElementById("content-display").textContent = event.target.result;
-    };
-    reader.readAsText(file);
-  }
+document.getElementById("mic").addEventListener("click", () => {
+    console.log("Mic clicked");
+});
+document.getElementById("replay").addEventListener("click", () => {
+    console.log("Replay clicked");
+});
+document.getElementById("save").addEventListener("click", () => {
+    console.log("Save clicked");
 });
