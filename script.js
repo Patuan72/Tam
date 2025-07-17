f2f53b9c131c8e496a773ecb4e49854f5f4f7f42
Patuan72 <ozoedtech@gmail.com>
2025-07-18 00:04:39 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('fileUpload').addEventListener('change', function(event) {
-  const file = event.target.files[0];
-  if (file) {
-    document.getElementById('fileName').innerText = file.name;
-  }
-});
