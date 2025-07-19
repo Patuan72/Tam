2f963215a5e3f40b4e64cb43939d650d7a5a6bcd
Patuan72 <ozoedtech@gmail.com>
2025-07-19 17:57:50 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,20 @@
-self.addEventListener("install", e => {
+self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open("v1").then(cache =>
-      cache.addAll(["index.html", "style.css", "script.js"])
-    )
+    caches.open('pwa-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/script.js'
+      ]);
+    })
   );
 });
-self.addEventListener("fetch", e => {
+
+self.addEventListener('fetch', function(e) {
   e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
   );
 });
