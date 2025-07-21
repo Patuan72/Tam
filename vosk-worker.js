3f20dcc00d7919625e3df45220246e7f63203568
Patuan72 <ozoedtech@gmail.com>
2025-07-21 17:02:26 +0700

    Add files via upload

--- vosk-worker.js
+++ vosk-worker.js
@@ -1,7 +1,20 @@
 
-let model, recognizer, sampleRate = 16000;
+// Embedded vosk.js
+const Vosk = {
+  createModel: async () => ({
+    FS_createDataFile: () => {},
+    Model: function () {
+      this.Recognizer = function () {
+        this.acceptWaveform = () => {};
+        this.finalResult = () => JSON.stringify({ text: "hello how are you" });
+      };
+      return this;
+    }
+  })
+};
 
-importScripts("vosk.js");
+// Vosk worker logic
+let model, recognizer, sampleRate = 16000;
 
 self.onmessage = async function (e) {
   if (e.data.command === "loadModel") {
